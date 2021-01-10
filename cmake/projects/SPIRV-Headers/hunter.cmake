a7435812228a3dce372dbbd74edd3ea69c40f21b
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2021-01-10 19:25:03 +0300

    Merge master branch of cpp-pm repo
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/projects/SPIRV-Headers/hunter.cmake
+++ cmake/projects/SPIRV-Headers/hunter.cmake
@@ -21,6 +21,28 @@ hunter_add_version(
     77018bfe6cb1eceaf824e401dbd206660a25bf66
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    SPIRV-Headers
+    VERSION
+    1.5.1.corrected
+    URL
+    https://github.com/KhronosGroup/SPIRV-Headers/archive/1.5.1.corrected.tar.gz
+    SHA1
+    e7911f27a13c61e9d8c13fe7b0df5f1a883e972e
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    SPIRV-Headers
+    VERSION
+    1.5.3.reservations1
+    URL
+    https://github.com/KhronosGroup/SPIRV-Headers/archive/1.5.3.reservations1.tar.gz
+    SHA1
+    e1666e3cf71e0eb8c75a1786e9f430f6d7670f42
+)
+
 hunter_cmake_args(
     SPIRV-Tools
     CMAKE_ARGS
--- cmake/projects/tsl_hat_trie/hunter.cmake
+++ cmake/projects/tsl_hat_trie/hunter.cmake
@@ -1,7 +1,5 @@
-# Copyright (c) 2016-2017, Ruslan Baratov
-# Copyright (c) 2020, Soramitsu Co., Ltd.
+# Copyright (c) 2016-2020, Rahul Sheth, Ruslan Baratov
 # All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
 
 # !!! DO NOT PLACE HEADER GUARDS HERE !!!
 
@@ -9,6 +7,7 @@ include(hunter_add_version)
 include(hunter_cacheable)
 include(hunter_download)
 include(hunter_pick_scheme)
+
 
 hunter_add_version(
     PACKAGE_NAME
@@ -19,6 +18,17 @@ hunter_add_version(
     "https://github.com/masterjedy/hat-trie/archive/4fdfc75e75276185eed4b748ea09671601101b8e.tar.gz"
     SHA1
     1b871083877ccbedb69ea5ecbffda3732d125eb7
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    tsl_hat_trie
+    VERSION
+    0.6.0-1739fa1-p0
+    URL
+    "https://github.com/cpp-pm/hat-trie/archive/v0.6.0-1739fa1-p0.tar.gz"
+    SHA1
+    ef3f4518d5463779088c1a6ae4772630e6e47b53
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
