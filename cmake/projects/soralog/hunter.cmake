9e087ab199f19c6eb6443ab0b547085d48cded3e
Dmitriy Khaustov <khaustov.dm@gmail.com>
2021-05-26 00:45:46 +0300

    Update Soralog to v0.0.7 (#29)
    * Update soralog to v0.0.7
    
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/projects/soralog/hunter.cmake
+++ cmake/projects/soralog/hunter.cmake
@@ -19,6 +19,12 @@ hunter_add_version(
     URL "https://github.com/soramitsu/soralog/archive/v0.0.6.tar.gz"
     SHA1 "67a03e76c8bfb5e72f295688d194015ca62e8ed1")
 
+hunter_add_version(
+    PACKAGE_NAME soralog
+    VERSION "0.0.7"
+    URL "https://github.com/soramitsu/soralog/archive/v0.0.7.tar.gz"
+    SHA1 "cf3ea3f959505be3b404b8a2b3a10314111976b2")
+
 hunter_cmake_args(
     soralog
     CMAKE_ARGS
