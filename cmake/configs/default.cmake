2f5bf7b6a60e67aef0176088ffe18c0742c5dfa0
Igor Egorov <igor@soramitsu.co.jp>
2021-07-14 12:07:55 +0300

    Update prometheus-cpp version to 0.12.3-p0
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -426,7 +426,7 @@ hunter_default_version(pluginlib VERSION 1.12.1-p0)
 hunter_default_version(poly2tri VERSION 1.0.0)
 hunter_default_version(polyclipping VERSION 4.8.8-p0) # for Assimp
 hunter_default_version(presentproto VERSION 1.0)
-hunter_default_version(prometheus-cpp VERSION 0.6.0-p2)
+hunter_default_version(prometheus-cpp VERSION 0.12.3-p0)
 hunter_default_version(protobuf-c VERSION 1.3.0-p1)
 hunter_default_version(pthread-stubs VERSION 0.4)
 hunter_default_version(pugixml VERSION 1.8.1)
