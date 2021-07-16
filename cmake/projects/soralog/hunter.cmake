59ecd99230466665d1f720ceb8cc92ab7a4a6c1c
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2021-07-16 12:21:15 +0300

    Update LibP2P and Soralog
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/projects/soralog/hunter.cmake
+++ cmake/projects/soralog/hunter.cmake
@@ -8,34 +8,17 @@ include(hunter_pick_scheme)
 
 hunter_add_version(
     PACKAGE_NAME soralog
-    VERSION "0.0.5"
-    URL  "https://github.com/soramitsu/soralog/archive/v0.0.5.tar.gz"
-    SHA1 "8b192d0e4cced8743b8a5e943cd0a89a5ad7939d"
+    VERSION "0.0.9"
+    URL "https://github.com/soramitsu/soralog/archive/v0.0.9.tar.gz"
+    SHA1 "a5df392c969136e9cb2891d7cc14e3e6d34107d6"
 )
-
-hunter_add_version(
-    PACKAGE_NAME soralog
-    VERSION "0.0.6"
-    URL "https://github.com/soramitsu/soralog/archive/v0.0.6.tar.gz"
-    SHA1 "67a03e76c8bfb5e72f295688d194015ca62e8ed1")
-
-hunter_add_version(
-    PACKAGE_NAME soralog
-    VERSION "0.0.7"
-    URL "https://github.com/soramitsu/soralog/archive/v0.0.7.tar.gz"
-    SHA1 "cf3ea3f959505be3b404b8a2b3a10314111976b2")
-
-hunter_add_version(
-    PACKAGE_NAME soralog
-    VERSION "0.0.8"
-    URL "https://github.com/soramitsu/soralog/archive/v0.0.8.tar.gz"
-    SHA1 "082a9621af199386b59bacce3f505abe2706c4f5")
 
 hunter_cmake_args(
     soralog
     CMAKE_ARGS
       TESTING=OFF
       EXAMPLES=OFF
+      EXPOSE_MOCKS=ON
 )
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
